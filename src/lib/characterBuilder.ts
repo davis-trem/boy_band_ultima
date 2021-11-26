edf13ba6dec982fd5798dcd401ff0152a50ef623
davis.trem <davis.trem@gmail.com>
2021-11-26 09:00:59 -0500

    refine damage taken

--- src/lib/characterBuilder.ts
+++ src/lib/characterBuilder.ts
@@ -7,7 +7,7 @@ class CharacterBuilder {
   private emitter: Events.EventEmitter;
   private battleBar: BattleBar;
   private CHARGING_BEAT_COUNT = 2;
-  private MAX_CHARGING_BEAT_COUNT = 4;
+  private MAX_DAMAGE_GIVEN = 0.15;
 
   constructor(scene: Scene, emitter: Events.EventEmitter, battleBar: BattleBar) {
     this.scene = scene;
@@ -109,14 +109,20 @@ class CharacterBuilder {
       if (attacking) return;
 
       if (chargingAttack) {
+        // Stop charging animation and do attack
         console.log({ percentageToBeat, beatsSinceCharging, targetBeat: this.CHARGING_BEAT_COUNT });
         char.setData({ attacking: true, percentageToBeat: null });
         char.play('character_kick');
+        this.battleBar.offsetPlayerValueBy(
+          this.calulateDamage(beatsSinceCharging.track, percentageToBeat),
+        );
+        console.log(this.calulateDamage(beatsSinceCharging.track, percentageToBeat));
         char.once('animationcomplete', () => {
           char.setData({ attacking: false, beatsSinceCharging: null });
           char.playAfterRepeat('character_idle');
         });
       } else {
+        // Play charging animation
         char.play('character_walk');
       }
       char.setData({ chargingAttack: !chargingAttack });
@@ -126,28 +132,36 @@ class CharacterBuilder {
       'TICK_PLAYED',
       ({ tickHasReachBeat, closestBeat, percentageToBeat }: TickPlayedData) => {
         char.setData({ percentageToBeat });
-        const [chargingAttack, beatsSinceCharging] = char.getData([
-          'chargingAttack',
-          'beatsSinceCharging',
-        ]);
+        let beatsSinceCharging = char.getData('beatsSinceCharging');
+        const chargingAttack = char.getData('chargingAttack');
         if (!chargingAttack) {
           return;
         }
 
-        if (beatsSinceCharging !== null && tickHasReachBeat) {
-          char.setData({ beatsSinceCharging: beatsSinceCharging + 1 });
-          console.log('beat played', beatsSinceCharging, closestBeat);
+        if (
+          tickHasReachBeat &&
+          beatsSinceCharging !== null &&
+          beatsSinceCharging.beatOnCharge !== closestBeat
+        ) {
+          char.setData({
+            beatsSinceCharging: {
+              track: beatsSinceCharging.track + 1,
+              beatOnCharge: beatsSinceCharging.beatOnCharge,
+            },
+          });
         }
         if (beatsSinceCharging === null) {
-          char.setData({ beatsSinceCharging: 1 });
-          console.log(beatsSinceCharging, closestBeat);
+          char.setData({ beatsSinceCharging: { track: 0, beatOnCharge: closestBeat } });
+          return;
         }
 
-        if (beatsSinceCharging > this.CHARGING_BEAT_COUNT && percentageToBeat > 0.5) {
+        // Get updated beatsSinceCharging since it changed in previous if
+        beatsSinceCharging = char.getData('beatsSinceCharging');
+        if (beatsSinceCharging.track >= this.CHARGING_BEAT_COUNT && percentageToBeat > 0.5) {
           char.setData({ chargingAttack: false });
           console.log('diiiiiie');
           char.play('character_die');
-          this.battleBar.offsetPlayerValueBy(-0.15);
+          this.battleBar.offsetPlayerValueBy(-this.MAX_DAMAGE_GIVEN);
           char.once('animationcomplete', () => {
             char.anims.reverse();
             char.setData({ attacking: false, beatsSinceCharging: null });
@@ -158,6 +172,18 @@ class CharacterBuilder {
     );
 
     return char;
+  }
+
+  private calulateDamage(beatsSinceChargingTrack: number, percentageToBeat: number): number {
+    // If over a beat away from target, take points away
+    if (Math.abs(this.CHARGING_BEAT_COUNT - beatsSinceChargingTrack) > 1) {
+      return -this.MAX_DAMAGE_GIVEN;
+    }
+
+    return (
+      (1 - Math.abs(this.CHARGING_BEAT_COUNT - (beatsSinceChargingTrack + percentageToBeat))) *
+      this.MAX_DAMAGE_GIVEN
+    );
   }
 }
 
